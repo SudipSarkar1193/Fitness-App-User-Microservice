53a6946f0bd718245f98c83ee9416d4858e37857
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-10-17 18:33:30 +0530

    Added size validation in RegisterUserRequestDTO || Added Global exception handler for MethodArgumentNotValidException

--- src/main/java/com/fitness/user/dtos/RegisterUserRequestDTO.java
+++ src/main/java/com/fitness/user/dtos/RegisterUserRequestDTO.java
@@ -1,15 +1,18 @@
 package com.fitness.user.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
 @Data
 public class RegisterUserRequestDTO {
     @NotBlank(message = "First name cannot be blank")
+    @Size(min=3, message="First name must be at least 3 characters long")
     private String firstName;
 
     @NotBlank(message = "Last name cannot be blank")
+    @Size(min=3, message="Last name must be at least 3 characters long")
     private String lastName;
 
     @NotBlank(message = "Email cannot be blank")
--- src/main/java/com/fitness/user/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/fitness/user/exceptions/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import com.fitness.user.dtos.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @ControllerAdvice // This annotation makes it a global exception handler
 @Slf4j
@@ -23,6 +25,24 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 ex.getMessage() == null ? "Bad request" : ex.getMessage()
         );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponseDTO> handleValidationException(MethodArgumentNotValidException ex){
+
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .reduce("", (msg1, msg2) -> msg1 + "; " + msg2);
+
+        ErrorResponseDTO errorResponse = new ErrorResponseDTO(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                errorMessage.isEmpty()?"Bad request":errorMessage
+        );
+
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
